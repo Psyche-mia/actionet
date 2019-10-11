57bec49b7510656abd8503536c98b2e324c3dbb0
jiafei1224 <51585075+jiafei1224@users.noreply.github.com>
2019-10-11 14:54:51 +0800

    Update user_controller.py

--- user_controller.py
+++ user_controller.py
@@ -6,12 +6,17 @@ import time
 controller = ai2thor.controller.Controller()
 controller.start(player_screen_width=640,
         player_screen_height=640)
+# controller1 = ai2thor.controller.Controller()
+# controller1.start()
+
 print("Floorplan:")
 floorplan= input('')
 controller.reset('FloorPlan'+floorplan)
 event = controller.step(dict(action='Initialize', gridSize=0.25,renderObjectImage="True"))
-
+# controller1.reset('FloorPlan'+floorplan)
+# event = controller1.step(dict(action='Initialize', gridSize=0.25,renderObjectImage="True"))
 # Show user task details
+# event = controller1.step(dict(action='ToggleMapView'))
 task = "Make coffee"
 print ("Task: " + task)
 
@@ -30,9 +35,10 @@ midtasks = []
 
 while True:
     # Add mid-level actions
+    anglehand=0
     temp1 =[]
-    midtask=['Navigate','Toast','Boil','Wash','Fry','Heat','Serve','Cook','Nill']
-    instructions1 ="0.Navigate 3.Wash 6.Serve\n" \
+    midtask=['Navigate','Toast','Boil','Wash','Fry','Heat','Serve','Cook','Nill','Robotic control']
+    instructions1 ="0.Navigate 3.Wash 6.Serve 9.Robotic Control\n" \
                    "1.Toast    4.Fry  7.Cook\n" \
                    "2.Boil     5.Heat 8.Finish\n"
     print("Middle level tasks:")
@@ -56,23 +62,53 @@ while True:
 
     temp = []
     while True:
+        anglehand=anglehand+30.0
         if keyboard.is_pressed('right'):
             event = controller.step(dict(action='MoveRight'))
+            # event = controller1.step(dict(action='MoveRight'))
             temp.append('MoveRight')
         elif keyboard.is_pressed('up'):
             event = controller.step(dict(action='MoveAhead'))
+            # event = controller1.step(dict(action='MoveAhead'))
             temp.append('MoveAhead')
+
         elif keyboard.is_pressed('down'):
             event = controller.step(dict(action='MoveBack'))
+            # event = controller1.step(dict(action='MoveBack'))
             temp.append('MoveBack')
+
         elif keyboard.is_pressed('left'):
             event = controller.step(dict(action='MoveLeft'))
+            # event = controller1.step(dict(action='MoveLeft'))
             temp.append('MoveLeft')
+
         elif keyboard.is_pressed('a'):
-            event = controller.step(dict(action='RotateLeft'))
+            # event = controller.step(dict(action='RotateLeft'))
+            # event = controller1.step(dict(action='RotateLeft'))
+            position=event.metadata['agent']['position']
+            rotation= event.metadata['agent']['rotation']
+            angle = rotation.get('y')
+            print(angle)
+            rotate=angle-30.0
+            x= position.get('x')
+            y= position.get('y')
+            z= position.get('z')
+            event = controller.step(dict(action='TeleportFull', x=x, y=y, z=z, rotation=rotate, horizon=0.0))
+
             temp.append('RotateLeft')
         elif keyboard.is_pressed('d'):
-            event = controller.step(dict(action='RotateRight'))
+            # event = controller.step(dict(action='RotateRight'))
+            # event = controller1.step(dict(action='RotateRight'))
+            position = event.metadata['agent']['position']
+            rotation = event.metadata['agent']['rotation']
+            angle = rotation.get('y')
+            print(angle)
+            rotate = angle + 30.0
+            x = position.get('x')
+            y = position.get('y')
+            z = position.get('z')
+            event = controller.step(dict(action='TeleportFull', x=x, y=y, z=z, rotation=rotate, horizon=0.0))
+
             temp.append('RotateRight')
         elif keyboard.is_pressed('w'):
             event = controller.step(dict(action='LookUp'))
@@ -80,6 +116,34 @@ while True:
         elif keyboard.is_pressed('s'):
             event = controller.step(dict(action='LookDown'))
             temp.append('LookDown')
+        elif keyboard.is_pressed('8'):
+            event = controller.step(dict(action='MoveHandAhead', moveMagnitude = 0.1))
+            temp.append('MoveHandAhead')
+        elif keyboard.is_pressed('5'):
+            event = controller.step(dict(action='MoveHandBack', moveMagnitude=0.1))
+            temp.append('MoveHandBack')
+        elif keyboard.is_pressed('4'):
+            event = controller.step(dict(action='MoveHandLeft', moveMagnitude=0.1))
+            temp.append('MoveHandLeft')
+        elif keyboard.is_pressed('6'):
+            event = controller.step(dict(action='MoveHandRight', moveMagnitude=0.1))
+            temp.append('MoveHandBack')
+        elif keyboard.is_pressed('9'):
+            event = controller.step(dict(action='MoveHandUp', moveMagnitude=0.1))
+            temp.append('MoveHandUp')
+        elif keyboard.is_pressed('3'):
+            event = controller.step(dict(action='MoveHandDown', moveMagnitude=0.1))
+            temp.append('MoveHandDown')
+        elif keyboard.is_pressed('7'):
+            event = controller.step(dict(action='RotateHand', x =anglehand))
+            temp.append('RotateHandX')
+        elif keyboard.is_pressed('1'):
+            event = controller.step(dict(action='RotateHand', z =anglehand))
+            temp.append('RotateHandZ')
+        elif keyboard.is_pressed('0'):
+            event = controller.step(dict(action='RotateHand', y =anglehand))
+            temp.append('RotateHandY')
+
         elif keyboard.is_pressed('o'):
             target_obj = input("\nName your target object to OPEN: ")
             for i in event.instance_detections2D:
